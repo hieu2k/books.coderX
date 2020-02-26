bd045d576d6faa21617faf2016d004bfddb1fd02
vuthanhieu <vuthanhieu2000@gmail.com>
2020-02-26 20:40:07 +0700

    setup basic

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) =>{
-    res.send('Hello world');
-})
+app.set('view engine', 'pug')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+//routers
+const homeRouter = require('./routers/home.route');
+
+app.use('/', homeRouter);
 
 app.listen(port, () => {
     console.log('Server connect with port: ', port);
